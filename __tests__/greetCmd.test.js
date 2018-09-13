77192d509b36b8b36b8d040ae3250bfb0f710fb5
utakuma <utatatata.kuma@gmail.com>
2018-09-13 20:27:18 +0900

    fix type error

--- __tests__/greetCmd.test.js
+++ __tests__/greetCmd.test.js
@@ -1,22 +1,22 @@
 /* @flow */
-import { GreetOption } from '../src/greetCmd'
+import type { GreetOption } from '../src/greetCmd'
 import greetCmd from '../src/greetCmd'
 
 describe('greetCmd', () => {
   it(`greetCmd({}) should be 'Hello!'`, () => {
-    const option: GreetOption = {}
+    const option: GreetOption = { yourName: null, goodEvening: null }
     expect(greetCmd(option)).toBe('Hello!')
   })
   it(`greetCmd({ yourName: 'Node CLI' }) should be 'Hello, Node CLI!'`, () => {
-    const option: GreetOption = { yourName: 'Node CLI' }
+    const option: GreetOption = { yourName: 'Node CLI', goodEvening: null }
     expect(greetCmd(option)).toBe('Hello, Node CLI!')
   })
   it(`greetCmd({ goodEvening: true }) should be 'Good Evening!'`, () => {
-    const option: GreetOption = { goodEvening: true }
+    const option: GreetOption = { yourName: null, goodEvening: true }
     expect(greetCmd(option)).toBe('Good Evening!')
   })
   it(`greetCmd({ goodEvening: true, yourName: 'Node CLI' }) should be 'Good Evening, Node CLI!'`, () => {
-    const option: GreetOption = { goodEvening: true, yourName: 'Node CLI' }
+    const option: GreetOption = { yourName: 'Node CLI', goodEvening: true }
     expect(greetCmd(option)).toBe('Good Evening, Node CLI!')
   })
 })
