b8d1d54b1e6be8c2e4e2fb101071e17661a62469
utakuma <utatatata.kuma@gmail.com>
2018-09-14 01:21:54 +0900

    use test instead of it in Jest and fix test case names

--- __tests__/greetCmd.test.js
+++ __tests__/greetCmd.test.js
@@ -3,20 +3,20 @@ import type { GreetOption } from '../src/greetCmd'
 import greetCmd from '../src/greetCmd'
 
 describe('greetCmd', () => {
-  it(`greetCmd({}) should be 'Hello!'`, () => {
+  test(`greetCmd({ goodEvening: null, yourName: null }) should be 'Hello!'`, () => {
     const option: GreetOption = { yourName: null, goodEvening: null }
     expect(greetCmd(option)).toBe('Hello!')
   })
-  it(`greetCmd({ yourName: 'Node CLI' }) should be 'Hello, Node CLI!'`, () => {
-    const option: GreetOption = { yourName: 'Node CLI', goodEvening: null }
+  test(`greetCmd({ goodEvening: null, yourName: 'Node CLI' }) should be 'Hello, Node CLI!'`, () => {
+    const option: GreetOption = { goodEvening: null, yourName: 'Node CLI' }
     expect(greetCmd(option)).toBe('Hello, Node CLI!')
   })
-  it(`greetCmd({ goodEvening: true }) should be 'Good Evening!'`, () => {
-    const option: GreetOption = { yourName: null, goodEvening: true }
+  test(`greetCmd({ goodEvening: true, yourName: null }) should be 'Good Evening!'`, () => {
+    const option: GreetOption = { goodEvening: true, yourName: null }
     expect(greetCmd(option)).toBe('Good Evening!')
   })
-  it(`greetCmd({ goodEvening: true, yourName: 'Node CLI' }) should be 'Good Evening, Node CLI!'`, () => {
-    const option: GreetOption = { yourName: 'Node CLI', goodEvening: true }
+  test(`greetCmd({ goodEvening: true, yourName: 'Node CLI' }) should be 'Good Evening, Node CLI!'`, () => {
+    const option: GreetOption = { goodEvening: true, yourName: 'Node CLI' }
     expect(greetCmd(option)).toBe('Good Evening, Node CLI!')
   })
 })
